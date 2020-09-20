f5ceaf5c7253d4a68d1168e66f6296e948d2b88b
Arash Izadi <arash.iz@live.com>
2020-09-20 02:57:59 -0400

    Tried to implement Damage but it was unsuccessful
    I need to use Box Collider

--- Bad Frogger/Assets/statusText.cs
+++ Bad Frogger/Assets/statusText.cs
@@ -9,8 +9,14 @@ public class statusText : MonoBehaviour
     public Text text;
     public int score;
     public int damage;
+    public int time;
     private float timer;
-    int time;
+    public Transform player;
+    public Transform enemyOne;
+    public Transform enemyTwo;
+    public Transform enemyThree;
+    public Transform enemyFour;
+    public Transform enemyFive;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +28,10 @@ public class statusText : MonoBehaviour
     {
         scoreText(score, damage);
         timeCount();
+        if (damage >= 5)
+        {
+            scoreText("Game Over");
+        }
     }
     public void scoreText(string _status)
     {
@@ -38,6 +48,20 @@ public class statusText : MonoBehaviour
         {
             timer = 0;
             time += 1;
+        }
+    }
+
+    public void damageCount()
+    {
+        if (
+            (Math.Round(enemyOne.position.x) == Math.Round(player.position.x) & Math.Round(enemyOne.position.y) == Math.Round(player.position.y)) ||
+            (Math.Round(enemyTwo.position.x) == Math.Round(player.position.x) & Math.Round(enemyTwo.position.y) == Math.Round(player.position.y)) ||
+            (Math.Round(enemyThree.position.x) == Math.Round(player.position.x) & Math.Round(enemyThree.position.y) == Math.Round(player.position.y)) ||
+            (Math.Round(enemyFour.position.x) == Math.Round(player.position.x) & Math.Round(enemyFour.position.y) == Math.Round(player.position.y)) ||
+            (Math.Round(enemyFive.position.x) == Math.Round(player.position.x) & Math.Round(enemyFive.position.y) == Math.Round(player.position.y))
+            )
+        {
+            damage += 1;
         }
     }
 }
