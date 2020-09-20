caf8eb59e1b2f46a5797dfae214fba2137adc24d
Arash Izadi <arash.iz@live.com>
2020-09-19 23:57:25 -0400

    Modified EnemyMovement to smooth things out

--- Bad Frogger/Assets/EnemyMovement.cs
+++ Bad Frogger/Assets/EnemyMovement.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
+using Random = System.Random;
 
 public class EnemyMovement : MonoBehaviour
 {
+    private bool leftToRightDirection;
     private float speed;
     public float _speed;
     private float timer;
@@ -14,18 +16,37 @@ public class EnemyMovement : MonoBehaviour
     {
         timer = 0.75f;
         _speed = 1f;
-        speed = Random.Range(1,5) * _speed;
+        speed = UnityEngine.Random.Range(1,5) * _speed;
+        { //New
+            var restoreYPosition = _transform.position.y;
+            var restoreZPosition = _transform.position.z;
+            _transform.position = new Vector3(UnityEngine.Random.Range(-4.00f, 4.00f), restoreYPosition, restoreZPosition);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= 0.7)
+        if (timer >= 2) //New timer >=2 | Old timer >=1
         {
+            Random rNG = new Random(); //New
             timer = 0;
-            _transform.position += new Vector3(Random.Range(-5.00f, 5.00f), 0, 0) * speed;
+            if (rNG.Next(100) < 50) //New
+                leftToRightDirection = true;
+            else
+                leftToRightDirection = false;
+
+
+
+            //_transform.position += new Vector3(UnityEngine.Random.Range(-5.00f, 5.00f), 0, 0) * speed;
         }
+        if (leftToRightDirection)
+            _transform.position += new Vector3(0.01f, 0, 0);
+        else
+            _transform.position -= new Vector3(0.01f, 0, 0);
+
         if (_transform.position.x > 10 || _transform.position.x < -10)
         {
             var restoreYPosition = _transform.position.y;
